f203f5b7d7439e64acd93c933ef75a88115fb6dc
Mauro Bianchi <bianchimro@gmail.com>
2015-07-09 16:12:04 +0200

    postgres

--- gtfsserver/gtfsserver/settings.py
+++ gtfsserver/gtfsserver/settings.py
@@ -83,11 +83,20 @@ WSGI_APPLICATION = 'gtfsserver.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gtfs',
+        'USER': 'gtfs',
+        'PASSWORD' : 'gtfs'
     }
 }
 
