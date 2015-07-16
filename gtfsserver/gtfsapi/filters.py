56c1c7eb39f40ee749f6dd14d27c7746bb620c6d
Mauro Bianchi <bianchimro@gmail.com>
2015-07-16 12:03:13 +0200

    closes #6

--- gtfsserver/gtfsapi/filters.py
+++ gtfsserver/gtfsapi/filters.py
@@ -3,12 +3,14 @@ from multigtfs.models import Trip, Service
 
 class TripFilter(django_filters.FilterSet):
     feed = django_filters.CharFilter(name="service__feed")
+    route_id = django_filters.CharFilter(name="route__route_id")
 
     """
     This filter is needed to lookup active trips by "feed" query parameter
     """
     class Meta:
         model = Trip
+        fields = ["feed", "route_id", "route"]
 
 
 class ServiceFilter(django_filters.FilterSet):
@@ -18,4 +20,4 @@ class ServiceFilter(django_filters.FilterSet):
 
     class Meta:
         model = Service
-        #fields = ["id", "feed", "feed__id"]
+        fields = ["id", "feed", "monday"]
