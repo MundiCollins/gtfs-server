e6c13db0550d074885ce5e1b35eaf6cee0ae4e20
Derick Lung'aho <derick.lungaho@gmail.com>
2016-02-21 18:47:08 +0300

    drop related_name attribute in the route relationship in the trip model. Fixes #17

--- gtfsserver/multigtfs/models/trip.py
+++ gtfsserver/multigtfs/models/trip.py
@@ -28,7 +28,7 @@ class Trip(Base):
     This implements trips.txt in the GTFS feed
     """
 
-    route = models.ForeignKey('Route', related_name="trips")
+    route = models.ForeignKey('Route')
     service = models.ForeignKey('Service', null=True, blank=True)
     trip_id = models.CharField(
         max_length=255, db_index=True,
